13048572f07fd1f9c71f8054e7e47b7b07b0c781
EscapedGibbon <maximgolubev2@gmail.com>
2022-03-14 12:29:55 +0100

    refactor: changed names of array variables

--- src/index.js
+++ src/index.js
@@ -10,38 +10,43 @@ const testing = [
   [255, 255, 255, 255, 255],
 ];
 function erode(image) {
-  const Arr = JSON.parse(JSON.stringify(image));
+  const imageCopy = JSON.parse(JSON.stringify(image));
 
-  const Arr2 = JSON.parse(JSON.stringify(image));
+  const imageCopy2 = JSON.parse(JSON.stringify(image));
 
-  for (let i = 1; i <= Arr.length - 2; ++i) {
-    for (let j = 1; j <= Arr.length - 2; ++j) {
+  for (let i = 1; i < imageCopy.length - 1; ++i) {
+    for (let j = 1; j < imageCopy.length - 1; ++j) {
       if (
-        Arr[i][j] === 255 &&
-        Arr[i - 1][j - 1] === 255 &&
-        Arr[i - 1][j] === 255 &&
-        Arr[i - 1][j + 1] === 255 &&
-        Arr[i][j + 1] === 255 &&
-        Arr[i + 1][j + 1] === 255 &&
-        Arr[i + 1][j] === 255 &&
-        Arr[i + 1][j - 1] === 255 &&
-        Arr[i][j - 1] === 255
+        imageCopy[i][j] === 255 &&
+        imageCopy[i - 1][j - 1] === 255 &&
+        imageCopy[i - 1][j] === 255 &&
+        imageCopy[i - 1][j + 1] === 255 &&
+        imageCopy[i][j + 1] === 255 &&
+        imageCopy[i + 1][j + 1] === 255 &&
+        imageCopy[i + 1][j] === 255 &&
+        imageCopy[i + 1][j - 1] === 255 &&
+        imageCopy[i][j - 1] === 255
       ) {
-        Arr2[i][j] = 255;
+        imageCopy2[i][j] = 255;
       } else {
-        Arr2[i][j] = 0;
+        imageCopy2[i][j] = 0;
       }
     }
   }
-  for (let i = 0; i <= Arr.length - 1; ++i) {
-    for (let j = 0; j <= Arr.length - 1; ++j) {
-      if (i === 0 || i === Arr.length - 1 || j === 0 || j === Arr.length - 1) {
-        Arr2[i][j] = 0;
+  for (let i = 0; i < imageCopy.length; ++i) {
+    for (let j = 0; j < imageCopy.length; ++j) {
+      if (
+        i === 0 ||
+        i === imageCopy.length - 1 ||
+        j === 0 ||
+        j === imageCopy.length - 1
+      ) {
+        imageCopy2[i][j] = 0;
       }
     }
   }
 
-  console.log(Arr2);
+  console.log(imageCopy2);
 }
 erode(testing);
 module.exports = erode;
